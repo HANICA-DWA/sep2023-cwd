f1f5b2a1de28d32e64c76edc8b7369c8b7ba5883
Fritz van Deventer <fritzvd@users.noreply.github.com>
2023-09-01 10:23:36 +0200

    promisify the mongo stuff

--- unit01 - HTTP en DOM/session 1.1/workshop/http_cats/app.js
+++ unit01 - HTTP en DOM/session 1.1/workshop/http_cats/app.js
@@ -1,133 +1,167 @@
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var xml         = require('xml');
-var path        = require('path');
-var morgan      = require('morgan');
-var compression = require('compression');
+const express = require("express");
+const bodyParser = require("body-parser");
+const xml = require("xml");
+const path = require("path");
+const morgan = require("morgan");
+const compression = require("compression");
+const { MongoClient, ObjectID } = require("mongodb");
 
-var app = express();
-app.set('view engine', 'ejs');
-app.set('json spaces', 3);
+const app = express();
+app.set("view engine", "ejs");
+app.set("json spaces", 3);
 
-app.use(morgan('dev'));
-app.use(compression({threshold: 1}));
+app.use(morgan("dev"));
+app.use(compression({ threshold: 1 }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'client-side')));
+app.use(express.static(path.join(__dirname, "client-side")));
 
-var mongo    = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
+const mongoUrl = "mongodb://127.0.0.1:27017/http_cats";
 
-mongo.connect('mongodb://127.0.0.1:27017/http_cats', function(err, db) {
-  if(err) throw err;
+async function startServer() {
+  const client = await MongoClient.connect(mongoUrl, {});
+  const db = client.db();
 
-  var collection = db.collection('pictures');
+  const collection = db.collection("pictures");
 
-  var pictureRoutes = express.Router();
-  // get the list of movies
+  const pictureRoutes = express.Router();
 
-  pictureRoutes.get('/', function(request, response) {
-      collection.find().toArray( function(err, resultArray){
-         if(err) throw err;
-         response.format({
-            text: function(){
-               response.status(406);
-               response.send('The server cannot render the requested content type. Accepted types are: \n  *  text/html\n  *  application/xml\n  *  application/json\n\n');
-            },
-            html: function(){
-               response.render("catlist.html.ejs", {catz:resultArray});
-            },
-            json: function(){
-               response.json(resultArray);
-            },
-            xml: function(){
-               response.render("catlist.xml.ejs", {catz:resultArray });
-            }
-         }); // end res.format
-      }); // end collection.find.toArray
-   }); // end router.get
-   // get a single movie
-   pictureRoutes.get('/:id', function(request, response) {
-      var theID = request.params.id
-      if(theID.length > 20) {
-         theID = ObjectId(theID)
+  pictureRoutes.get("/", async (request, response) => {
+    try {
+      const resultArray = await collection.find().toArray();
+      response.format({
+        text: () => {
+          response.status(406);
+          response.send(
+            "The server cannot render the requested content type. Accepted types are: \n  *  text/html\n  *  application/xml\n  *  application/json\n\n"
+          );
+        },
+        html: () => {
+          response.render("catlist.html.ejs", { catz: resultArray });
+        },
+        json: () => {
+          response.json(resultArray);
+        },
+        xml: () => {
+          response.render("catlist.xml.ejs", { catz: resultArray });
+        },
+      });
+    } catch (err) {
+      throw err;
+    }
+  });
+
+  pictureRoutes.get("/:id", async (request, response) => {
+    try {
+      const theID =
+        request.params.id.length > 20
+          ? ObjectID(request.params.id)
+          : request.params.id;
+      const pictureInfo = await collection.findOne({ _id: theID });
+      if (!pictureInfo) {
+        response.status(404);
+        response.send("No cat with that ID.\n\n");
+        return;
       }
-      collection.findOne({_id:theID}, function(err, pictureInfo) {
-         if(err) throw err;
-         if(pictureInfo == null) {
-            response.status(404);
-            response.send('No cat with that ID.\n\n');
-            return;
-         }
-         response.format({
-            text: function(){
-               response.status(406);
-               response.send('The server cannot render the requested content type. Accepted types are: \n  *  text/html\n  *  application/xml\n  *  application/json\n\n');
-            },
-            html: function(){
-               response.render("cat.html.ejs", {cat:pictureInfo});
-            },
-            json: function(){
-               response.json(pictureInfo);
-            },
-            xml: function(){
-               response.render("cat.xml.ejs", {theCat:pictureInfo });
-            }
-         }); // end res.format
-      }); // end findOne
-   }); // end route.get
-
-  pictureRoutes.post('/', function(request, response) {
-     request.body.created_at = new Date();
-     collection.insert(request.body, function(err, result) {
-        if(err) throw err;
-        response.json(request.body);
-     });
+      response.format({
+        text: () => {
+          response.status(406);
+          response.send(
+            "The server cannot render the requested content type. Accepted types are: \n  *  text/html\n  *  application/xml\n  *  application/json\n\n"
+          );
+        },
+        html: () => {
+          response.render("cat.html.ejs", { cat: pictureInfo });
+        },
+        json: () => {
+          response.json(pictureInfo);
+        },
+        xml: () => {
+          response.render("cat.xml.ejs", { theCat: pictureInfo });
+        },
+      });
+    } catch (err) {
+      throw err;
+    }
   });
 
-  pictureRoutes.put('/:id', function(request, response) {
-     delete request.body._id; // body-parser made _id a string. Mongo does not like that.
-     collection.update({_id:request.params.id}, request.body, function(err, result) {
-        if(err) throw err;
-        request.body[_id] = request.params.id;
-        response.json({ok:true, result: request.body[_id]});
-     });
+  pictureRoutes.post("/", async (request, response) => {
+    try {
+      request.body.created_at = new Date();
+      const result = await collection.insertOne(request.body);
+      response.json(request.body);
+    } catch (err) {
+      throw err;
+    }
   });
 
-  pictureRoutes.delete('/:id', function(request, response) {
-     collection.remove({_id: request.params.id}, function(err,result){
-        if(err) throw err;
-        response.json({ok:true});
-     });
+  pictureRoutes.put("/:id", async (request, response) => {
+    try {
+      delete request.body._id;
+      const result = await collection.updateOne(
+        { _id: request.params.id },
+        { $set: request.body }
+      );
+      response.json({ ok: true, result: request.params.id });
+    } catch (err) {
+      throw err;
+    }
   });
-  app.use("/catz",pictureRoutes);
 
-   function fillEmptyDatabase() {
-      collection.find().toArray( function(err, resultArray){
-         if(err) throw err;
-         if( resultArray.length == 0) {
-            var catz = [ {submitter: "Theo Theunissen", imageURL: "https://bit.ly/1hXk1oa", upVotes: 0},
-                         {submitter: "Robert Holwerda", imageURL: "https://i.chzbgr.com/maxW500/8552422912/h65EE8BB6/", upVotes: 0},
-                         {submitter: "Lars Tijsma",     imageURL: "https://i.chzbgr.com/maxW500/8552414976/hF2587F16/", upVotes: 0}
-                      ];
-            for( var idx in catz ) {
-               catz[idx]._id = idx;
-               var callbackCounter = 0;
-               collection.insert(catz[idx], function(err, result) {
-                  if(err) throw err;
-                  if(callbackCounter==idx) {
-                    console.log( "Database was empty: added", parseInt(idx)+1, "LOLcats to the database called \"http_cats\".");
-                  } else {
-                    callbackCounter++
-                  }
-               }); // end insert
-            } // end for
-         } // end if
-     }); // end
-  } // end function
-   fillEmptyDatabase(); // call fillEmptyDatabase immediately
-});
+  pictureRoutes.delete("/:id", async (request, response) => {
+    try {
+      const result = await collection.deleteOne({ _id: request.params.id });
+      response.json({ ok: true });
+    } catch (err) {
+      throw err;
+    }
+  });
 
-app.listen(3000);
-console.log("Server running on port 3000.");
+  app.use("/catz", pictureRoutes);
+
+  async function fillEmptyDatabase() {
+    try {
+      const resultArray = await collection.find().toArray();
+
+      if (resultArray.length === 0) {
+        const catz = [
+          {
+            submitter: "Theo Theunissen",
+            imageURL: "https://bit.ly/1hXk1oa",
+            upVotes: 0,
+          },
+          {
+            submitter: "Robert Holwerda",
+            imageURL: "https://i.chzbgr.com/maxW500/8552422912/h65EE8BB6/",
+            upVotes: 0,
+          },
+          {
+            submitter: "Lars Tijsma",
+            imageURL: "https://i.chzbgr.com/maxW500/8552414976/hF2587F16/",
+            upVotes: 0,
+          },
+        ];
+
+        for (let idx in catz) {
+          catz[idx]._id = idx;
+          await collection.insertOne(catz[idx]);
+          console.log(
+            "Database was empty: added",
+            parseInt(idx) + 1,
+            'LOLcats to the database called "http_cats".'
+          );
+        }
+      }
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  fillEmptyDatabase().catch((err) => console.error(err));
+
+  app.listen(3000);
+  console.log("Server running on port 3000.");
+}
+
+startServer().catch((err) => console.error(err));
